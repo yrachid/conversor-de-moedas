62bf7fc92808a678637ef928b3309ec11675d79d
brendamoniqueb <bmbrocardo@gmail.com>
2019-03-06 16:07:08 -0300

    modificações

--- src/main/java/com/aceleradora/conversor/Conversor.java
+++ src/main/java/com/aceleradora/conversor/Conversor.java
@@ -2,44 +2,12 @@ package com.aceleradora.conversor;
 
 public class Conversor {
 
-    public String converter(String entrada) {
-
-        String codigoMoedaOriginal = entrada.substring(0, 3);
-
-        int indiceEspaco = entrada.indexOf(" ");
-        String codigoMoedaDestino = entrada.substring(indiceEspaco + 1, indiceEspaco + 4);
-
-        if (!moedaDisponivel(codigoMoedaOriginal)) {
-            return "EnumMoeda indisponivel: " + codigoMoedaOriginal;
-        }
-
-        if (!moedaDisponivel(codigoMoedaDestino)) {
-            return "EnumMoeda indisponivel: " + codigoMoedaDestino;
-        }
-
-        return entrada;
-    }
-
-    private boolean moedaDisponivel(String codigoMoeda) {
-
-        Moeda[] moedasDisponiveis = Moeda.values();
-
-        for (int i = 0; i < moedasDisponiveis.length; i++) {
-            if (moedasDisponiveis[i].getCodigo().equals(codigoMoeda)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
 
     public double retornaValor(String entrada){
 
         int indiceEspaco = entrada.indexOf(" ");
 
         String valor = entrada.substring(3, indiceEspaco);
-
-        new Double("6.35");
 
         double valorFinal = Double.parseDouble(valor);
 
--- src/main/java/com/aceleradora/conversor/Moeda.java
+++ src/main/java/com/aceleradora/conversor/Moeda.java
@@ -17,4 +17,46 @@ public enum Moeda {
         return codigo;
     }
 
-}
+
+
+        public static Moeda valorPorCodigo(String codigoMoeda) throws Exception {
+            for (Moeda moeda : values()) {
+                if (moeda.getCodigo().equals(codigoMoeda)){
+                    return moeda;
+                }
+            }
+            throw new Exception("Moeda não suportada" +  codigoMoeda);
+        }
+    public static String validador(String entrada) {
+
+        String codigoMoedaOriginal = entrada.substring(0, 3);
+
+        int indiceEspaco = entrada.indexOf(" ");
+        String codigoMoedaDestino = entrada.substring(indiceEspaco + 1, indiceEspaco + 4);
+
+        if (!moedaDisponivel(codigoMoedaOriginal)) {
+            return "EnumMoeda indisponivel: " + codigoMoedaOriginal;
+        }
+
+        if (!moedaDisponivel(codigoMoedaDestino)) {
+            return "EnumMoeda indisponivel: " + codigoMoedaDestino;
+        }
+
+        return entrada;
+    }
+
+    private static boolean moedaDisponivel(String codigoMoeda) {
+
+        Moeda[] moedasDisponiveis = Moeda.values();
+
+        for (int i = 0; i < moedasDisponiveis.length; i++) {
+            if (moedasDisponiveis[i].getCodigo().equals(codigoMoeda)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+        }
+
+
